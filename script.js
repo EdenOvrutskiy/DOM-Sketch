178d4f7b0e2d18ce7ba9d375e5acb6616f8d3d6b
Eden Ovrtuskiy <none>
2022-06-20 22:19:56 -0400

    added event listener

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 //todo:
-//make outer-most lines as bold as the middle
+//create a function that changes the color of each square
 
-//listen to events on each grid and make it change color
-
-
-//create a 16x16 grid of square divs.
-
-//create a container for all divs
+//create a container for all grid divs
 containerDiv = document.querySelector('.container');
 
-for (i = 0; i < 16; ++i) {
+//create a 16x16 grid of square divs.
+for (let i = 0; i < 16; ++i) {
     const rowDiv = createRowContainer();
     fillRow(rowDiv);
 }
-
 
 function createRowContainer() { //to separate each row from one another
     const rowDiv = document.createElement('div');
@@ -26,42 +21,28 @@ function fillRow(rowDiv) {//create 16 divs and place into row
     for (let i = 0; i < 16; ++i) {
         //create a div
         singleDiv = document.createElement('div');
-        //give it some height via content to see it
-        //singleDiv.textContent = 'x';
-        //give it class of "row-element"
-        //singleDiv.classList.add('row-element');
         //nest in container
         rowDiv.appendChild(singleDiv);
+        //listen to hovers
+        listenToHover(singleDiv);
     }
 }
 
-//outer sides of the grid are less bold, becaue they
-//are only touching a single border.
+//listen to events
+//addEventListener to each .div on creation
 
-//I could select them manually to bolden to border..
+function listenToHover(div) {
+    div.addEventListener('mouseover', onHover);
+}
 
-//or I could set some property such that it can be applied
-//to each square
+function onHover(eventObject) {
+    console.log(eventObject);
+    eventObject.classList.add('.hovered');
+    //color the event object
+}
 
-//some if statemet to check if it's the first in the row?
+//create a function that changes the color of each square
+//maybe create a css class with transition and apply like
+//the javascript drumkit thing
+//maybe no transition at first, just a color change.
 
-//it's hard to inject that into the code..
-
-//the first in each row need border-left
-//the last in each row need border-right
-
-//all first row needs border-top
-
-//all bottom row needs border-bottom
-
-//maybe border-box property can help things?
-
-//margin maybe?
-
-//maybe I can just google "intersecting borders"
-
-//outline property?
-
-//create gaps between all of them so the overlap isn't obvious?
-
-//maybe the grid css property?
